2306d12b9f1505986ed173a06c55d5a88eb60e2e
c0derboy <per3ian.killer.recover@gmail.com>
2021-01-05 16:35:24 +0330

    V1.0

---
+++ app.py
@@ -0,0 +1,11 @@
+prime_numbers = [2, ]
+stop = 100
+
+for n in range(2, stop):
+	for i in prime_numbers:
+		if n%i != 0:
+			prime_numbers.append(n)
+			break
+		break
+print(prime_numbers)
+print(f'There are {len(prime_numbers)} prime numbers until {stop}')
