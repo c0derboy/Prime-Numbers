2503f408312ae3a3453a2e1dd19e47a126d855c0
c0derboy <per3ian.killer.recover@gmail.com>
2021-01-05 17:55:47 +0330

    V1.12

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import datetime
+
+stop = input('Specify the range of numbers. (Use Ctrl + C if Lagged)')
 start_time = datetime.datetime.now()
 prime_numbers = [2]
-stop = 100000
 
 for n in range(3, int(stop)):
 	is_prime = True
@@ -16,7 +17,7 @@ for n in range(3, int(stop)):
 
 print(prime_numbers)
 print(f'There are {len(prime_numbers)} prime numbers until {stop}')
-print('This is V1.1')
+print('This is V1.12')
 
 end_time = datetime.datetime.now()
 duartion = (end_time - start_time).total_seconds() * 1000
